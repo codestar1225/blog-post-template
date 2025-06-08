012836cef3f4a8ced2f30601f012e1e0d906bf29
codestar1225 <reactexpert1996@gmail.com>
2025-06-08 18:05:14 +0900

    feat: home

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <>
+      <header className="flex justify-end py-5 px-5">
+        <button className="rounded-lg border-foreground border px-3">Log In by Google</button>
+      </header>
+    </>
+  );
+};
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Footer from "./_components/footer";
+import Header from "./_components/header";
+
+const Page = () => {
+  return (
+    <>
+      <Header />
+      <main className="flex h-svh items-center justify-center flex-col gap-5">
+        <h1 className="text-[28px]">Welcome to my blog post platform today!</h1>
+        <div>Built by Vitalii B.   8 June 2025</div>
+      </main>
+    </>
+  );
+};
+export default Page;
